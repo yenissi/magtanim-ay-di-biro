b8ea3de58a680031d9a33001085f00c45503c609
yenissi <gelassi13@gmail.com>
2025-01-21 21:08:32 +0800

    day one commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { SafeAreaView, Text, View } from 'react-native'
+import LogIn from '../app/LogIn'
+
+export class index extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <LogIn/>
+      </SafeAreaView>
+    )
+  }
+}
+
+export default index
